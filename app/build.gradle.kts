a4f4e4a6e32716f3dc4b0c6fabd0a02c5b1c30dc
mocihan <goreliu@126.com>
2026-06-03 11:08:43 +0800

    Update minSdk version to 28
    Changed minSdk version from 31 to 28.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,7 +79,7 @@ android {
 
     defaultConfig {
         applicationId = "lastchat.rikkafork.cocolal"
-        minSdk = 31
+        minSdk = 28
         targetSdk = 36
         versionCode = ((System.currentTimeMillis() - 1577808000000) / 60000).toInt() // 基于 2020-01-01 00:00:00 UTC 的分钟数
         val baseVersionName = "1.4.5"
